ad233c65448493ffa03f810d208147e4f0dd0cda
Bruno Silva <135295017+brunolk1@users.noreply.github.com>
2024-05-31 14:01:57 -0300

    editando a secion quem-somos

--- tata.recreacao/src/app/app.component.ts
+++ tata.recreacao/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './shared/header/header.component';
 import { QuemSomosComponent } from './shared/quem.somos/quem.somos.component';
+import { MainComponent } from './shared/main/main.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent,QuemSomosComponent ],
+  imports: [HeaderComponent,QuemSomosComponent, MainComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
