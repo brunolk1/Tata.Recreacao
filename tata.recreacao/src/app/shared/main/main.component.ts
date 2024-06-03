bba2201e1be4fc1b1e87ad1329e4c63ca88871bc
Bruno Silva <135295017+brunolk1@users.noreply.github.com>
2024-06-03 13:32:50 -0300

    fazendo o caminho para que esteja funcionando

--- tata.recreacao/src/app/shared/main/main.component.ts
+++ tata.recreacao/src/app/shared/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SectionSobreComponent } from './section-sobre/section-sobre.component';
+import { SectionOndeComponent } from './section-onde/section-onde.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [SectionSobreComponent],
+  imports: [SectionSobreComponent, SectionOndeComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
