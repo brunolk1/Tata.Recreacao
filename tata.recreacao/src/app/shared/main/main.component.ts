9dd1dc3c768876fdd51c5aa8f2793eaabb29e3fd
Bruno Silva <135295017+brunolk1@users.noreply.github.com>
2024-06-04 18:41:36 -0300

    fazendo a comunicação entre os components

--- tata.recreacao/src/app/shared/main/main.component.ts
+++ tata.recreacao/src/app/shared/main/main.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { SectionSobreComponent } from './section-sobre/section-sobre.component';
 import { SectionOndeComponent } from './section-onde/section-onde.component';
+import { SectionBrincadeirasComponent } from './section-brincadeiras/section-brincadeiras.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [SectionSobreComponent, SectionOndeComponent],
+  imports: [SectionSobreComponent, SectionOndeComponent, SectionBrincadeirasComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
