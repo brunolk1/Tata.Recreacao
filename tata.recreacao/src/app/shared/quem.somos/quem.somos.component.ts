80962b6b7fe4848f31193229dd9653e066d2b21f
Bruno Silva <135295017+brunolk1@users.noreply.github.com>
2024-06-11 11:17:13 -0300

    fiz a modificação no nav e modifiquei algumas coisas que achei interessante no component brincadeiras

--- tata.recreacao/src/app/shared/quem.somos/quem.somos.component.ts
+++ tata.recreacao/src/app/shared/quem.somos/quem.somos.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { AnimationQuemComponent } from '../animation-quem/animation-quem.component';
+import { BotaoComponent } from '../botao/botao.component';
 
 @Component({
   selector: 'quem-somos',
   standalone: true,
-  imports: [HeaderComponent, AnimationQuemComponent],
+  imports: [HeaderComponent, AnimationQuemComponent, ],
   templateUrl: './quem.somos.component.html',
   styleUrl: './quem.somos.component.scss'
 })
