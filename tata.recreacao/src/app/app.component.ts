c4331370dd51543015f53859eca8607577d2b10a
Bruno Silva <135295017+brunolk1@users.noreply.github.com>
2024-06-11 11:13:59 -0300

    fiz a edição do caminho no ts para exibir os novos components

--- tata.recreacao/src/app/app.component.ts
+++ tata.recreacao/src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './shared/header/header.component';
 import { QuemSomosComponent } from './shared/quem.somos/quem.somos.component';
 import { MainComponent } from './shared/main/main.component';
+import { BotaoComponent } from './shared/botao/botao.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent,QuemSomosComponent, MainComponent ],
+  imports: [HeaderComponent,QuemSomosComponent, MainComponent, BotaoComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
